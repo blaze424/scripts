20654cbcc5bc181a2fa47a9134559f90d747ab23
blaze <secndary.123@gmail.com>
2025-01-23 20:56:33 +0530

    Added script.
    Added PixelOS 15 build script.

---
+++ pixelos.sh
@@ -0,0 +1,31 @@
+rm -rf .repo/local_manifests/
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs
+
+git clone -b pixelos-15-qpr1 --depth 1 https://github.com/blaze424/local_manifests .repo/local_manifests
+
+echo =====
+echo Local Manifest Cloning Successfull
+echo =====
+/opt/crave/resync.sh
+
+echo =====
+echo Sync Successfull
+echo =====
+
+export BUILD_USERNAME=blaze
+export BUILD_HOSTNAME=crave
+export TZ=Asia/India
+echo ======= Export Done ======
+
+source build/envsetup.sh
+echo ==================
+echo envsetup.sh done
+echo ==================
+ 
+echo ==================
+echo Starting Build
+echo ==================
+
+breakfast sweet user
+ 
+mka bacon -j$(nproc --all)
