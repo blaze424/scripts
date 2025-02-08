cf0e9dd31fc1b4161bbfb72baf6b7f32c2649313
blaze <secndary.123@gmail.com>
2025-02-08 19:16:14 +0530

    Update pixelos.sh

--- pixelos.sh
+++ pixelos.sh
@@ -3,24 +3,24 @@ repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs
 
 git clone -b pixelos-15-qpr1 --depth 1 https://github.com/blaze424/local_manifests .repo/local_manifests
 
-echo =====
+echo **********************************
 echo Local Manifest Cloning Successfull
-echo =====
+echo **********************************
 /opt/crave/resync.sh
 
-echo =====
+echo ****************
 echo Sync Successfull
-echo =====
+echo ****************
 
 source build/envsetup.sh
-echo ==================
-echo envsetup.sh done
-echo ==================
- 
-echo ==================
-echo Starting Build
-echo ==================
 
-breakfast sweet user
+echo ****************
+echo envsetup.sh done
+echo ****************
+
+echo **************
+echo Starting Build
+echo **************
+
 mka installclean
-mka bacon
+brunch sweet user
