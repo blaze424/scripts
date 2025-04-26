f0eb4f2d5c260638de3924bc080e02dee40ddb00
ablazry <secndary.123@gmail.com>
2025-04-26 23:47:21 +0530

    Initial commit: Build script file for PixelBuildsROM

---
+++ pb.sh
@@ -0,0 +1,24 @@
+rm -rf .repo/local_manifests/
+repo init -u https://github.com/PixelBuildsROM/android_manifest -b infinity
+
+git clone -b pb --depth 1 https://github.com/blaze424/local_manifests .repo/local_manifests
+
+echo "***************"
+echo Local Manifest Cloning Successfull
+echo "***************"
+
+/opt/crave/resync.sh
+echo "***************"
+echo Sync Successfull
+echo "***************"
+
+source build/envsetup.sh
+echo "***************"
+echo envsetup.sh done
+echo "***************"
+ 
+echo "***************"
+echo Starting Build
+echo "***************"
+
+./pb-build.sh dubai
