1b95d2c01a2dce0b24b1bb5a2938c6fb6e33abfe
ablazry <secndary.123@gmail.com>
2025-04-27 11:08:11 +0530

    remove dir: hardware/qcom-caf/common

--- pb.sh
+++ pb.sh
@@ -1,4 +1,6 @@
 rm -rf .repo/local_manifests/
+rm -rf hardware/qcom-caf/common/
+
 repo init -u https://github.com/PixelBuildsROM/android_manifest -b infinity
 
 git clone -b pb --depth 1 https://github.com/blaze424/local_manifests .repo/local_manifests
