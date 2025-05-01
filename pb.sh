01f41bdaf505993f65bc54f992b47af57c0f1559
blaze <secndary.123@gmail.com>
2025-05-01 10:44:19 +0530

    Update pb.sh

--- pb.sh
+++ pb.sh
@@ -1,24 +1,24 @@
-rm -rf .repo/local_manifests/
-repo init -u https://github.com/PixelBuildsROM/android_manifest -b infinity
+#rm -rf .repo/local_manifests/
+#repo init -u https://github.com/PixelBuildsROM/android_manifest -b infinity
 
-git clone -b pb-temp --depth 1 https://github.com/blaze424/local_manifests .repo/local_manifests
+#git clone -b pb-temp --depth 1 https://github.com/blaze424/local_manifests .repo/local_manifests
 
-echo "***************"
-echo Local Manifest Cloning Successfull
-echo "***************"
+#echo "***************"
+#echo Local Manifest Cloning Successfull
+#echo "***************"
 
-/opt/crave/resync.sh
-echo "***************"
-echo Sync Successfull
-echo "***************"
+#/opt/crave/resync.sh
+#echo "***************"
+#echo Sync Successfull
+#echo "***************"
 
-source build/envsetup.sh
-echo "***************"
-echo envsetup.sh done
-echo "***************"
+#source build/envsetup.sh
+#echo "***************"
+#echo envsetup.sh done
+#echo "***************"
  
-echo "***************"
-echo Starting Build
-echo "***************"
+#echo "***************"
+#echo Starting Build
+#echo "***************"
 
 ./pb-build.sh dubai
